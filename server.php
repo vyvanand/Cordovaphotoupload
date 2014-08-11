e9a45182fd0e590a58cf3e909a71a9b12e523932
vyvanand <vyvanand@gmail.com>
2014-08-11 12:01:33 +0530

    Create server.php

---
+++ server.php
@@ -0,0 +1,13 @@
+<?php
+  $path = realpath($_SERVER["DOCUMENT_ROOT"]);
+  include("mysql_connection.php");
+  $current_date=date("Y-m-d");
+  $dirname = "$path/client_upload/assets/user_uploaded_images/"; 
+	if ($_FILES)
+	{
+	    print_r($_FILES);	
+	    mkdir ($dirname, 0777, true); 
+	    move_uploaded_file($_FILES["file"]["tmp_name"],$dirname."/".$current_date."_".$_FILES["file"]["name"]."");
+	    $p_dir="".$dirname."/".$current_date."_".$_FILES["file"]["name"]."";
+  }
+?>
